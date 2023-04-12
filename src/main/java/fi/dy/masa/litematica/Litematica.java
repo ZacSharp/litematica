4be1c41f74335d53369dd46741f7a22dbe848b41
ZacSharp <68165024+ZacSharp@users.noreply.github.com>
2023-04-13 01:47:45 +0200

    Make the config gui accessible via the mod list

--- src/main/java/fi/dy/masa/litematica/Litematica.java
+++ src/main/java/fi/dy/masa/litematica/Litematica.java
@@ -8,7 +8,9 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fmlclient.ConfigGuiHandler.ConfigGuiFactory;
 import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
+import fi.dy.masa.litematica.gui.GuiConfigs;
 import fi.dy.masa.malilib.event.InitializationHandler;
 
 @Mod(Reference.MOD_ID)
@@ -26,6 +28,22 @@ public class Litematica
         // Make sure the mod being absent on the other network side does not cause
         // the client to display the server as incompatible
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (incoming, isNetwork) -> true));
+
+        // Make the "Config" button in the mod list open the config gui
+        ModLoadingContext.get().registerExtensionPoint(ConfigGuiFactory.class, () -> new ConfigGuiFactory((mc, parent) -> {
+                GuiConfigs gui = new GuiConfigs() {
+                        @Override
+                        public void render(com.mojang.blaze3d.vertex.PoseStack matrixStack, int mouseX, int mouseY, float partialTicks)
+                        {
+                            matrixStack.translate(0, 0, -1);
+                            parent.render(matrixStack, 0, 0, partialTicks);
+                            matrixStack.translate(0, 0, 1);
+                            super.render(matrixStack, mouseX, mouseY, partialTicks);
+                        }
+                };
+                gui.setParentGui(parent);
+                return gui;
+        }));
 
 //        MinecraftForge.EVENT_BUS.register(new ForgeInputEventHandler());
 //        MinecraftForge.EVENT_BUS.register(new ForgeRenderEventHandler());
